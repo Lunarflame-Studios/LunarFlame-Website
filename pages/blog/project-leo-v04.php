f6d362bb07086816ec3311aba659905f2a9dfb6d
Phantomforce260 <120600016+Phantomforce260@users.noreply.github.com>
2024-04-08 19:10:49 -0400

    Add test php file

---
+++ pages/blog/project-leo-v04.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Play Nexopshere v0.4</title>
+        <link rel="stylesheet" href="../../style.css">
+        <link rel="stylesheet" href="blog-page.css">
+        <link rel="stylesheet" href="../../assets/font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+       <section class="sub-header">
+            <nav>
+                <a href="../../index.html"><img src="../../images/Lunarflame_Logo.png"></a>
+                <div class="nav-links header" id="navLinks">
+                    <i class="fa fa-times" onclick="hideMenu()"></i>
+                    <ul>
+                        <li><a href="../../index.html">HOME</a></li>
+                        <li><a href="../about.html">ABOUT</a></li>
+                        <li><a href="../projects.html">PROJECTS</a></li>
+                        <li><a href="../blog.html">BLOG</a></li>
+                    </ul>
+                </div>
+                <i class="fa fa-bars" onclick="showMenu()"></i>
+            </nav>
+        </section>
+
+        <section class="page-body">
+            <div id="overlay"></div>
+            <img id="zoom-in" src="" alt="">
+
+            <div class="blog-title">
+                <h1 id="title">Play Nexosphere v0.4</h1>
+                <h5 id="category">Nexosphere</h5>
+                <hr>
+            </div>
+            <div class="blog-subInfo">
+                <h3 id="author">Phantom</h3>
+                <h4 id="date">Apr 08, 2024</h4>
+            </div>
+
+        </section>
+    </body>
+</html>
