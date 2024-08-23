2459ddd2ebd4517d9171bc4588a4b59238698cd3
Phantom <120600016+Phantomforce260@users.noreply.github.com>
2024-08-23 15:33:13 -0400

    Revert "js image fix test"
    This reverts commit 2ce4ddc5752ff72c2f5b11feb564ffe94dd2fbda.

--- javascript/home.js
+++ javascript/home.js
@@ -1,10 +1,9 @@
 const header = document.querySelector('.home-header');
-const images = ['images/PL_SS_1.png', 'images/PL_SS_4.png', 'images/Horizon_Skyline_1.png', 'images/PL_SS_5.png'];
-//const imagePaths = ['images/PL_SS_1.png', 'images/PL_SS_4.png', 'images/Horizon_Skyline_1.png', 'images/PL_SS_5.png'];
+const imagePaths = ['images/PL_SS_1.png', 'images/PL_SS_4.png', 'images/Horizon_Skyline_1.png', 'images/PL_SS_5.png'];
 let currentIndex = 0;
 
 function changeBackground() {
-    header.style.backgroundImage = `linear-gradient(rgba(5, 18, 70, 0.7), rgba(59, 4, 70, 0.7)), url(${images[currentIndex]})`;
+    header.style.backgroundImage = `linear-gradient(rgba(5, 18, 70, 0.7), rgba(59, 4, 70, 0.7)), url(${imagePaths[currentIndex]})`;
 }
 
 function nextSlide() {
