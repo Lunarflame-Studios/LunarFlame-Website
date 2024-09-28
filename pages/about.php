d19558436b9d4d33cdb0a048e35393c492941109
Phantomforce260 <120600016+Phantomforce260@users.noreply.github.com>
2024-09-27 20:59:30 -0400

    about page changes pt 1

--- pages/about.php
+++ pages/about.php
@@ -10,44 +10,82 @@
 <body>
     <?php require('partials/sub-header.php')?>
 
+    <!-- &#8203; is an empty ACSII character. -->
+
     <section class="page-body">
         <div id="overlay"></div>
         <img id="zoom-in" src="" alt="">
 
         <div class="about-general">
-            <h1>About Us</h1>
-            <p>Lunarflame Studios is an independent game studio founded in 2023.</p>
-            <img class="page-image gallery-img" src="../images/PL_SS_1.png" alt="">
-            <p>Our studio strives to create games with thought-provoking stories, immersion for countless audiences,
-                foster artistic expression, and have fun while doing it all.</p>
+            <h1 class="typewrite" data-type='["About Us", "About Our Team", "About Our Vision"]' data-period="2000">
+                <span class="wrap">&#8203;</span>
+            </h1>
+
+            <p class="typewriter-v2"> &#8203; 
+                <span>Lunarflame Studios is an independent game studio founded in 2023.</span>
+            </p>
+
+            <img class="page-image gallery-img offset-border" src="../images/PL_SS_1.png" alt="">
+
+            <p class="typewriter-v2"> &#8203;
+                <span>Our studio strives to create games with thought-provoking stories, immersion for countless audiences,
+                foster artistic expression, and have fun while doing it all.</span>
+            </p>
         </div>
+
         <hr>
-        <h1>Developers</h1><br><br>
+
+        <h1 class="typewriter-v2"> &#8203;
+            <span>Developers</span>
+        </h1>
+
+        <br><br>
+
         <div class="about-devs">
             <div class="about-dev phantom">
                 <div class="text-group">
                     <hr class="hr-odd">
-                    <h2>Adrian</h2>
-                    <h4>Phantom</h4>
-                    <h5>Lead Artist, Lead Programmer</h5>
+
+                    <h2 class="typewriter-v2"> &#8203;
+                        <span>Adrian</span>
+                    </h2>
+
+                    <h4 class="typewriter-v2"> &#8203;
+                        <span>Phantom</span>
+                    </h4>
+
+                    <h5 class="typewriter-v2"> &#8203;
+                        <span>Lead Artist, Lead Programmer</span>
+                    </h5>
+
                     <p>"I've always been a huge believer in the idea that video games are the future of storytelling. 
                         I'm a jRPG fiend, some of my favorite titles include Persona, Xenoblade, and Nier.
                         I began this project because I wanted to create my own story to tell to the world."</p>
-                    <!--<a href="" class="hero-btn">Read More</a>-->
+                    <a href="" class="hero-btn">Read More</a>
                 </div>
                 <div class="pfp-phantom"><img src="../images/adri_pfp.jpg" alt=""></div>
             </div>
             <div class="about-dev kapeepa">
                 <div class="text-group">
                     <hr class="hr-even">
-                    <h2>Daniel</h2>
-                    <h4>Kapeepa</h4>
-                    <h5>Lead Musician, Programmer</h5>
+
+                    <h2 class="typewriter-v2"> &#8203;
+                        <span>Daniel</span>
+                    </h2>
+
+                    <h4 class="typewriter-v2"> &#8203;
+                        <span>Kapeepa</span>
+                    </h4>
+
+                    <h5 class="typewriter-v2"> &#8203;
+                        <span>Lead Musician, Programmer</span>
+                    </h5>
+
                     <p>
                         "Certified learner. I joined the project to get good at programming and music stuff. 
-                        Like Adrian, I do love me some video games---so much we can learn from them. PM sleeper agent."
+                        Like Adrian, I do love me some video games---so much we can learn from them. PM sleeper agent (!)"
                     </p>
-                    <!--<a href="" class="hero-btn">Read More</a>-->
+                    <a href="" class="hero-btn">Read More</a>
                 </div>
                 <div class="pfp-kapeepa"><img src="../images/dan_pfp.png" alt=""></div>
             </div>
